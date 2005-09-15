eb6a5cf131f688a9aeb9c45cc0a55153fba3dccf
Robby Russell <robby@planetargon.com>
2005-09-15 05:07:40 +0000

    Let's try this again

--- svnwatch.rb
+++ svnwatch.rb
@@ -2,21 +2,18 @@ require 'drb'
 
 class SvnWatch < Plugin
 
-  
   def help(plugin)
     m.reply "svnwatch start (starts the drb instance)"
   end
   
-  def privmsg(m)
-  
+  def privmsg(m)  
     unless(m.params)
       m.reply "Incorrect usage. " + help(m.plugin)
     end
-    
   end
 
   def post(str)
-    m.say "#pdx.rb", str
+    @bot.say "#pdx.rb",  str
   end  
   
 end
@@ -28,4 +25,5 @@ svnwatch.register("svnwatch")
 # start the DRb instance
 DRb.start_service('druby://localhost:7666', svnwatch)
 DRb.thread.join
+
 
