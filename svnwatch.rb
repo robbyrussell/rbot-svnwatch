9fe71d2b85394dea443153d3cb85c0dcda2e72cb
Robby Russell <robby@planetargon.com>
2005-09-15 05:24:46 +0000

    attempting to move the drb instance into the object

--- svnwatch.rb
+++ svnwatch.rb
@@ -7,23 +7,35 @@ class SvnWatch < Plugin
   end
   
   def privmsg(m)  
-    unless(m.params)
+  
+  unless(m.params)
       m.reply "Incorrect usage. " + help(m.plugin)
     end
+
+    if m.params == "start"
+      start_up  
+    end
+
   end
 
   def post(str)
     @bot.say "#pdx.rb",  str
   end  
   
+  private
+  def start_up
+    
+    # start the DRb instance
+    DRb.start_service('druby://localhost:7666', self)
+    DRb.thread.join
+  end
+    
 end
 
 # register with rbot
 svnwatch = SvnWatch.new
 svnwatch.register("svnwatch")
 
-# start the DRb instance
-DRb.start_service('druby://localhost:7666', svnwatch)
-DRb.thread.join
+
 
 
